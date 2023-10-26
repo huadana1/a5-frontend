0d91c09652834c323e07fdfee7168b633e0596dd
Dana Hua <huadana1@gmail.com>
2023-10-26 18:44:33 -0400

    login and resource not found pages styling

--- client/components/Login/LoginForm.vue
+++ client/components/Login/LoginForm.vue
@@ -27,7 +27,12 @@ async function login() {
         <input type="password" v-model.trim="password" id="aligned-password" placeholder="Password" required />
       </div>
       <div class="pure-controls">
-        <button type="submit" class="pure-button pure-button-primary">Submit</button>
+        <button type="submit" class="pure-button pure-button-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check2" viewBox="2 -3 16 16">
+              <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+          </svg>
+          Submit
+        </button>
       </div>
     </fieldset>
   </form>
--- client/views/NotFoundView.vue
+++ client/views/NotFoundView.vue
@@ -1,7 +1,16 @@
 <template>
   <main>
-    <h1>Not Found</h1>
+    <h1>Resource Not Found</h1>
     <p>The resource that you requested could not be found.</p>
-    <RouterLink to="/" class="button"> Return to the home page </RouterLink>
+    <RouterLink to="/" class="pure-button pure-button-primary"> Return to the home page </RouterLink>
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
